7af8aa2cedebd42b8c31bab479abff0bf74c528f
Uri Kalish <uri.kalish@microfocus.com>
2020-04-26 20:18:50 +0300

    update index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { goCapture, OutputType, CapturerOutput, CaptureOptions } from './capturer';
 
+export { CaptureOptions, OutputType } from './capturer';
+export { LogLevel } from './logger';
 export const capture = (
     outputType: OutputType,
     htmlDocument: HTMLDocument,
